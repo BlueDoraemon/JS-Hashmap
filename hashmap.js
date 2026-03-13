19c4def6c7e83768ddc8eded617bc40985ec49e9
Lixang Li <gtlx54@gmail.com>
2026-03-14 00:48:02 +1100

    feat: testing test.js

--- hashmap.js
+++ hashmap.js
@@ -1,56 +1,124 @@
+export default HashMap;
+
 class HashMap {
-    constructor(loadFactor = 0.75,capacity=17) {
-        this.loadFactor = loadFactor;
-        this.capacity = capacity;
-        this.size = 0;
-        this.buckets = Array.from({ length: capacity }, () => []);
-    }
-    cache = [this.capacity]; // init 17
-    hash(key, capacity = 17) {
-        let hashCode = 0;
-        key = String(key);
-        const PRIME_NUMBER = 31;
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.size = 0;
+    this.buckets = Array.from({ length: capacity }, () => []);
+  }
 
-        for (let i =0; i<key.length; i++) {
-            hashCode = PRIMENUMBER * hashCode + key.charCodeAt(i);
-            hashCode = hashCode % capacity;
-        }
+  hash(key) {
+    key = String(key);
+    let hashCode = 0;
+    const PRIME_NUMBER = 31;
 
-        return hashCode;
-
-    }
-    set(key,value) {
-        if (this.has(key)) {
-            cache[this.hash(key)] = value; //overwrite
-        }
-        else cache[this.hash(key)] = value; 
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (hashCode * PRIME_NUMBER + key.charCodeAt(i)) % this.capacity;
     }
 
-    get(key) {
-        return this.has(key) ? this.cache[this.hash(key)] : null;
-    }
-    has(key) {
-        return this.cache[this.hash(key)] ? true : false;
-    }
-    remove(key) {
+    return hashCode;
+  }
 
-    }
-    length() {
+  set(key, value) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
 
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        pair[1] = value;
+        return;
+      }
     }
-    clear() {
 
-    }
-    keys() {
+    bucket.push([key, value]);
+    this.size++;
 
+    if (this.size / this.capacity > this.loadFactor) {
+      this.resize();
     }
-    values(){
+  }
 
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let [k, v] of bucket) {
+      if (k === key) return v;
     }
-    // returns aan array contains each key,value as [[firstkey,firstvalue],[secondkey,secondvalue]]
-    entries()
+
+    return null;
+  }
+
+  has(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let [k] of bucket) {
+      if (k === key) return true;
+    }
+
+    return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        this.size--;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = Array.from({ length: this.capacity }, () => []);
+    this.size = 0;
+  }
+
+  keys() {
+    const result = [];
+    for (const bucket of this.buckets) {
+      for (const [k] of bucket) result.push(k);
+    }
+    return result;
+  }
+
+  values() {
+    const result = [];
+    for (const bucket of this.buckets) {
+      for (const [, v] of bucket) result.push(v);
+    }
+    return result;
+  }
+
+  entries() {
+    const result = [];
+    for (const bucket of this.buckets) {
+      for (const pair of bucket) result.push(pair);
+    }
+    return result;
+  }
+
+  resize() {
+    const oldBuckets = this.buckets;
+    this.capacity *= 2;
+    this.buckets = Array.from({ length: this.capacity }, () => []);
+    this.size = 0;
+
+    for (const bucket of oldBuckets) {
+      for (const [k, v] of bucket) {
+        this.set(k, v);
+      }
+    }
+  }
 }
-
-function 
-
-function 
